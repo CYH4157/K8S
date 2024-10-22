8feb65ecbe258e6f71babf7d6f12dafa3084e807
CYH4157 <a9756666@gmail.com>
2024-10-23 01:57:28 +0800

    [update] container setting

--- vmware-ubuntu22-04.sh
+++ vmware-ubuntu22-04.sh
@@ -31,10 +31,16 @@ sudo apt install containerd.io
 
 containerd config default | sudo tee /etc/containerd/config.toml
 
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
 sudo systemctl start containerd
 sudo systemctl enable containerd
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+
 
 sudo apt update
 sudo apt install -y kubelet kubeadm kubectl
