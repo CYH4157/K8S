9b9dfea6434e19ed3bda9cd94e986286a6845bfd
CYH4157 <a9756666@gmail.com>
2024-10-23 00:49:29 +0800

    [Add] vmware vcf env k8s

---
+++ vmware-ubuntu22-04.bash
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo swapoff -a
+# The purpose is to comment out the swap entry in /etc/fstab
+sudo sed -i '/swap/s/^/#/' /etc/fstab
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo sysctl --system
+
+sudo apt install ca-certificates curl
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install containerd.io
+
+containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo systemctl start containerd
+sudo systemctl enable containerd
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+
+# Set packages to not update automatically
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo apt-get install socat
+
+sudo apt-get update
+sudo apt-get install -y containerd
+
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+sudo systemctl status containerd
+
+echo "=============================================="
+echo "----- kubernetes installation is complete ----"
